f8a3d22e7b11fa5328836691bef27496e4417cbf
guywhale <guywhale@gmail.com>
2022-01-12 01:31:38 +0000

    Add styles for buttons with skewed outlines and hover effects

--- dev/components/sample/sample.php
+++ dev/components/sample/sample.php
@@ -5,7 +5,7 @@
         <h2>Heading 2</h2>
         <h3>Heading 3</h3>
         <h4>Heading 4</h4>
-        <h5>Heading 1</h5>
+        <h5>Heading 5</h5>
         <p>Body</p>
         <strong class="emphasise">emphasise</strong>
         <ul class="emphasise">
@@ -13,7 +13,48 @@
             <li>List 2</li>
             <li>List 3</li>
         </ul>
-        <a href="/" class="button">Button</a>
+        <div class="row">
+            <div class="back-grey-1 py-5 my-2 col-md-6">
+                <a href="/" class="button button--white button--medium">
+                    <span class="outline"></span>
+                    <span class="section-bg-color"></span>
+                    <span class="text">Normal</span>
+                </a>
+            </div>
+            <div class="back-grey-1 py-5 my-2 col-md-6">
+                <a href="/" class="button button--purple button--medium">
+                    <span class="outline"></span>
+                    <span class="section-bg-color"></span>
+                    <span class="text">Normal</span>
+                </a>
+            </div>
+            <div class="back-grey-1 py-5 my-2 col-md-6">
+                <a href="/" class="button button--purple button--small">
+                    <span class="outline"></span>
+                    <span class="section-bg-color"></span>
+                    <span class="text">Small</span>
+                </a>
+            </div>
+            <div class="back-grey-1 py-5 my-2 col-md-6">
+                <a href="/" class="button button--purple button--medium">
+                    <span class="outline"></span>
+                    <span class="section-bg-color"></span>
+                    <span class="text">Medium</span>
+                </a>
+            </div>
+            <div class="back-grey-1 py-5 my-2 col-md-6">
+                <a href="/" class="button button--purple button--large">
+                    <span class="outline"></span>
+                    <span class="section-bg-color"></span>
+                    <span class="text">Large</span>
+                </a>
+            </div>
+            <div class="back-grey-1 py-5 my-2 col-md-6">
+                <a href="/" class="button button--text-link">
+                    Text button
+                </a>
+            </div>
+        </div>
     </div>
 </div>
 
