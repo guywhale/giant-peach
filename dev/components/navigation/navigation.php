552ca909ea59050a9bea6dc907ef93c3f1f53d44
guywhale <guywhale@gmail.com>
2022-01-15 17:17:33 +0000

    Finish nav menu styles across all viewports

--- dev/components/navigation/navigation.php
+++ dev/components/navigation/navigation.php
@@ -27,10 +27,62 @@
                 'container_class' => 'navigation__menu-container',
             ]); ?>
             <div class="navigation__bottom-row">
-                <a href="" class="navigation__social-link"></a>
-                <a href="" class="navigation__social-link"></a>
-                <a href="" class="navigation__social-link"></a>
-                <a href="" class="navigation__social-link"></a>
+                <a href="https://en-gb.facebook.com/"
+                    class="navigation__social-link navigation__social-link--facebook"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <span class="blob">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/fb-shape.svg', [
+                            'keepcolors' => true,
+                        ]); ?>
+                    </span>
+                    <span class="logo logo--facebook">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/facebook-f.svg'); ?>
+                    </span>
+                </a>
+                <a href="https://twitter.com/"
+                    class="navigation__social-link navigation__social-link--twitter"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <span class="blob">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/twitter-shape.svg', [
+                            'keepcolors' => true,
+                        ]); ?>
+                    </span>
+                    <span class="logo logo--twitter">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/twitter.svg'); ?>
+                    </span>
+                </a>
+                <a href="https://www.linkedin.com/"
+                    class="navigation__social-link navigation__social-link--linkedin"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <span class="blob">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/linkedin-shape.svg', [
+                            'keepcolors' => true,
+                        ]); ?>
+                    </span>
+                    <span class="logo logo--linkedin">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/linkedin-in.svg'); ?>
+                    </span>
+                </a>
+                <a href="https://www.instagram.com/"
+                    class="navigation__social-link navigation__social-link--instagram"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <span class="blob">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/instagram-shape.svg', [
+                            'keepcolors' => true,
+                        ]); ?>
+                    </span>
+                    <span class="logo logo--instagram">
+                        <?= \Light\svg('/wp-content/uploads/2022/01/instagram.svg'); ?>
+                    </span>
+                </a>
             </div>
         </div>
     </div>
