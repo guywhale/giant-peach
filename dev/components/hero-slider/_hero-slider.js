60091659abfcb9bb914cf0bc0e8bec6cb8239433
guywhale <guywhale@gmail.com>
2022-01-16 14:27:11 +0000

    Set up hero slider styles

---
+++ dev/components/hero-slider/_hero-slider.js
@@ -0,0 +1,9 @@
+jQuery($ => {
+    $('.hero-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        dotsClass: 'slick-dots container',
+    });
+});
---
+++ dev/components/hero-slider/hero-slider.php
@@ -0,0 +1,38 @@
+<div class="hero-slider">
+    <div class="hero-slider__slide back-green-1 purple-1">
+        <img src="/wp-content/uploads/2022/01/sider-bg-desktop-v2.png"
+            alt="man in office"
+            class="hero-slider__slide-bg-img"
+        >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-xl-7">
+                    <p class="hero-slider__text caption">
+                        A <strong class="line-animation expand">proven</strong> formula to reinvigorate and advance your business.
+                    </p>
+                    <?= Light\render('build/components/button', [
+                        'type'           => 'white',
+                        'sectionBgColor' => 'green-1',
+                        'size'           => 'medium',
+                        'link'           => '/',
+                        'label'          => 'Learn more',
+                    ]);?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hero-slider__slide back-pink-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7"></div>
+            </div>
+        </div>
+    </div>
+    <div class="hero-slider__slide back-purple-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7"></div>
+            </div>
+        </div>
+    </div>
+</div>
