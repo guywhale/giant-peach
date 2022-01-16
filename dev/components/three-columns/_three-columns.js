85bfec32643229bbf6669c0635cf51fbd70ee4f1
guywhale <guywhale@gmail.com>
2022-01-16 21:29:27 +0000

    Animate title, subtitle and underline in three-columns section

---
+++ dev/components/three-columns/_three-columns.js
@@ -0,0 +1,57 @@
+jQuery($ => {
+    function inView(elem) {
+        const docViewTop = $(window).scrollTop();
+        const docViewBottom = docViewTop + $(window).height();
+        const elemTop = $(elem).offset().top;
+        const elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    $(window).on('scroll', () => {
+        $('.three-columns__section-title').each(function (i) {
+            if (inView(this) === true) {
+                const lineAnimate = $(this).find('.line-animation');
+
+                setTimeout(
+                    () => {
+                        $(this).addClass('animate');
+                    },
+                    i * 100
+                );
+
+                setTimeout(
+                    () => {
+                        lineAnimate.addClass('expand');
+                    },
+                    600
+                );
+            }
+        });
+
+        $('.three-columns__subtitle').each(function (i) {
+            if (inView(this) === true) {
+                setTimeout(
+                    () => {
+                        $(this).addClass('animate');
+                    },
+                    i * 100
+                );
+            }
+        });
+
+        $('.three-columns__row').each(function (i) {
+            if (inView(this) === true) {
+                const whiteBoxes = $(this).find('.three-columns__white-box');
+                console.log(whiteBoxes);
+
+                setTimeout(
+                    () => {
+                        $(this).addClass('animate');
+                    },
+                    i * 100
+                );
+            }
+        });
+    });
+});
