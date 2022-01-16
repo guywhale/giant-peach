12cfcf161f5761fe568d2a4914d2e17b7e3e8901
guywhale <guywhale@gmail.com>
2022-01-16 15:17:54 +0000

    Add line animation effect

--- dev/components/hero-slider/_hero-slider.js
+++ dev/components/hero-slider/_hero-slider.js
@@ -3,7 +3,34 @@ jQuery($ => {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
-        arrows: true,
+        nextArrow: $('.hero-slider__next'),
         dotsClass: 'slick-dots container',
+    });
+
+    let lineAnimation = $('.slick-current').find('.line-animation');
+
+    if (lineAnimation) {
+        lineAnimation.addClass('expand');
+    }
+
+    $('.hero-slider').on('afterChange', () => {
+        setTimeout(
+            () => {
+                lineAnimation = $('.slick-current').find('.line-animation');
+
+                if (lineAnimation) {
+                    lineAnimation.addClass('expand');
+                }
+            },
+            200
+        );
+    });
+
+    $('.hero-slider').on('beforeChange', () => {
+        lineAnimation = $('.slick-current').find('.line-animation');
+
+        if (lineAnimation) {
+            lineAnimation.removeClass('expand');
+        }
     });
 });
--- dev/components/hero-slider/hero-slider.php
+++ dev/components/hero-slider/hero-slider.php
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-lg-8 col-xl-7">
                     <p class="hero-slider__text caption">
-                        A <strong class="line-animation expand">proven</strong> formula to reinvigorate and advance your business.
+                        A <strong class="line-animation">proven</strong> formula to reinvigorate and advance your business.
                     </p>
                     <?= Light\render('build/components/button', [
                         'type'           => 'white',
@@ -21,18 +21,53 @@
             </div>
         </div>
     </div>
-    <div class="hero-slider__slide back-pink-1">
+    <div class="hero-slider__slide back-green-1 purple-1">
+        <img src="/wp-content/uploads/2022/01/sider-bg-desktop-v2.png"
+            alt="man in office"
+            class="hero-slider__slide-bg-img"
+        >
         <div class="container">
             <div class="row">
-                <div class="col-lg-7"></div>
+                <div class="col-lg-8 col-xl-7">
+                    <p class="hero-slider__text caption">
+                        A <strong class="line-animation">proven</strong> formula to reinvigorate and advance your business.
+                    </p>
+                    <?= Light\render('build/components/button', [
+                        'type'           => 'white',
+                        'sectionBgColor' => 'green-1',
+                        'size'           => 'medium',
+                        'link'           => '/',
+                        'label'          => 'Learn more',
+                    ]);?>
+                </div>
             </div>
         </div>
     </div>
-    <div class="hero-slider__slide back-purple-1">
+    <div class="hero-slider__slide back-green-1 purple-1">
+        <img src="/wp-content/uploads/2022/01/sider-bg-desktop-v2.png"
+            alt="man in office"
+            class="hero-slider__slide-bg-img"
+        >
         <div class="container">
             <div class="row">
-                <div class="col-lg-7"></div>
+                <div class="col-lg-8 col-xl-7">
+                    <p class="hero-slider__text caption">
+                        A <strong class="line-animation">proven</strong> formula to reinvigorate and advance your business.
+                    </p>
+                    <?= Light\render('build/components/button', [
+                        'type'           => 'white',
+                        'sectionBgColor' => 'green-1',
+                        'size'           => 'medium',
+                        'link'           => '/',
+                        'label'          => 'Learn more',
+                    ]);?>
+                </div>
             </div>
         </div>
     </div>
+</div>
+<div class="hero-slider__next-container container">
+    <button class="hero-slider__next">
+        <?= \Light\svg('/wp-content/uploads/2022/01/arrow-right.svg'); ?>
+    </button>
 </div>
